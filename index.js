86a9895cba36a6b0b4138c0a48ae2d980825813b
Carolin Eriksson <carolineriksson@MacBook-Air-som-tillhor-Carolin.local>
2024-10-24 13:09:45 +0200

    First outcast

---
+++ index.js
@@ -0,0 +1,52 @@
+/* //"My name is
+Carolin"; //Don`t work*/
+
+`My name is Carolin
+and I am  
+a developer.`; //This works with backtics!
+
+/* 
+var firstName = "Carolin";
+console.log(firstName);
+
+var firstName = "Ella";
+console.log(firstName);
+
+firstName = "Tuva";
+console.log(firstName); 
+
+var lastName
+console.log(lastName);//Don`t use var!!
+
+const age= 39;
+console.log(age)
+
+age=45; */
+
+/* const MyfirstName = "Carolin";
+
+if(true) {
+    const lastName = "Eriksson"
+    console.log(MyfirstName); 
+}
+
+console.log(MyfirstName);
+console.log(lastName); */
+
+// ############## let ##############
+/* let streetName = "tegelvägen";
+streetName = "Drottninggatan";
+
+let id = "182882!!+";
+id = 93832; */
+
+
+// ########### if statment ##################
+/*const age = 20;
+
+ if (age > 18){
+    console.log ("you are an adult");
+} else {
+    console.log("you are a child");
+} */
+
